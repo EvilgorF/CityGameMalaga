0bf99aca5c12785e7aa04a005df1b536dd9a85ed
EvilgorF <tdewolf2009@gmail.com>
2026-04-11 23:11:17 +0200

    hotfix: fixed url to swap html

--- script/home.js
+++ script/home.js
@@ -1,9 +1,11 @@
 let url = window.location.origin
-
+if (url.includes(`github.com`)) {
+    url = `${window.location.origin}/CityGameMalaga`
+}
 document.getElementById("info").addEventListener("click", () => {
     window.location.href = `${url}/info.html`;
 });
-document.getElementById("pin").addEventListener("click",  () => {
+document.getElementById("pin").addEventListener("click", () => {
     window.location.href = `${url}/index.html`;
 });
 document.getElementById("admin").addEventListener("click", () => {
